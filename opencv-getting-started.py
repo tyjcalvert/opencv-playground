7086f8f8a786aa6a08708af05e7b3cf8b8afc430
Tyler Calvert <tyjcalvert@gmail.com>
2025-04-04 15:02:45 -0500

    added logic to wait for q to quit

--- opencv-getting-started.py
+++ opencv-getting-started.py
@@ -2,9 +2,21 @@ import cv2
 
 image = cv2.imread('field.png', 0)
 
+print("Image shape:", image.shape)
+print("Image size:", image.size)
+
 window1 = cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.imshow(window1, image)
-cv2.waitKey(0)
+
+alive = True
+
+print("Press 'q' to quit the window")
+
+while alive:
+  cv2.imshow(window1, image)
+  keypress = cv2.waitKey(1)
+  if keypress == ord('q'):
+    alive = False
+
 cv2.destroyAllWindows()
 
 
